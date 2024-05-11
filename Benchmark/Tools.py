223a2e1b13a12912946722751836a95bd13714a1
jjlems <j.j.lems@student.tudelft.nl>
2024-05-11 13:25:25 +0200

    Calculated average crust thickness from moho

--- Benchmark/Tools.py
+++ Benchmark/Tools.py
@@ -8,10 +8,13 @@ import matplotlib.pyplot as plt
 topoDataType = np.dtype("int16")
 topoDataType = topoDataType.newbyteorder(">")
 
-topoFileName = "Data/Earth2014.BED2014.1min.geod.bin" #  (Hirt and Rexer, 2015)
-maskFileName = "Data/MSK2014_landtypes.1min.geod.bin" #  (Hirt and Rexer, 2015)
+topoFileName = "Data/Earth2014.BED2014.1min.geod.bin"  # (Hirt and Rexer, 2015)
+maskFileName = "Data/MSK2014_landtypes.1min.geod.bin"  # (Hirt and Rexer, 2015)
 
-# compensationDepth =  #  [m]
+meanContinental = 33.6  # [km], (Reguzzoni and Sampietro, 2015)
+meanOceanic = 12.9  # [km], (Reguzzoni and Sampietro, 2015)
+
+compensationDepth = 19.2e3  # [m]
 
 def readTopo():
     # reads topography data from file and returns np array
@@ -26,8 +29,12 @@ def getLandMask():
     data[data == 2] = 0
     return data
 
-if __name__ == "__main__":
+def calculateAverageThickness():
     mask = getLandMask()
-    plt.imshow(mask, cmap="hot")
-    plt.colorbar()
-    plt.show()
+    mask[mask == 0] = meanOceanic
+    mask[mask == 1] = meanContinental
+    meanThickness = np.mean(mask)
+    print("The mean moho thickness is: " + str(meanThickness) + " [km].")
+
+if __name__ == "__main__":
+    calculateAverageThickness()
