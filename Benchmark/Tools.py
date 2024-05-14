b5896200eee2689bcde456c90bd55b1a72a6d578
jjlems <j.j.lems@student.tudelft.nl>
2024-05-14 15:08:54 +0200

    Added function to decrease the resolution of the topography dataset to be consistent with moho dataset

--- Benchmark/Tools.py
+++ Benchmark/Tools.py
@@ -33,6 +33,15 @@ def readTopo():
     data = np.fromfile(topoFileName, dtype=topoDataType, count=-1)
     return np.flip(np.reshape(data, (2160, 4320)), 0)
 
+def decreaseTopoResolution(topo, resolution):
+    # decreases the resolution by taking the mean value of 2:1 rectangles of such size that
+    # the desired resolution is achieved
+    if (resolution[0] > topo.shape[0] or resolution[1] > topo.shape[1] or resolution[1] / resolution[0] != 2 or
+            topo.shape[0] % resolution[0] > 1e6 or topo.shape[1] % resolution[1] > 1e6):
+        raise ValueError("Original resolution (2160, 4320) should be divisible by new resolution")
+    return (topo.reshape(resolution[0], int(topo.shape[0] / resolution[0]), resolution[1],
+                         int(topo.shape[1] / resolution[1])).mean(axis=1).mean(axis=2))
+
 def getLandMask():
     # reads the land mask
     data = np.fromfile(maskFileName, dtype=topoDataType, count=-1)
@@ -67,6 +76,11 @@ def readMoho():
 
 if __name__ == "__main__":
     moho = readMoho()
+    topo = decreaseTopoResolution(readTopo(), moho.shape) / 1e3
+    fig1 = plt.figure()
     plt.imshow(moho, cmap="hot")
+    plt.colorbar()
+    fig2 = plt.figure()
+    plt.imshow(topo, cmap="hot")
     plt.colorbar()
     plt.show()
