e35a3e7ad70c7dc6489e5c4cdbea8de2c29840a7
jjlems <j.j.lems@student.tudelft.nl>
2024-06-05 15:00:21 +0200

    Restructured directories

---
+++ GSH-main/GSH-main/Thesis/Benchmark/GridSearch.m
@@ -0,0 +1,97 @@
+clear;
+close all;
+clc;
+clearvars;
+
+addpath('..\..\Tools');
+addpath('..\SharedTools');
+
+topoFileName = "..\..\..\..\Data\Earth2014.BED2014.5min.geod.bin";  % (Hirt and Rexer, 2015)
+mohoFileName = "..\..\..\..\Data\MohoDepthSzwillusEtAl2019";  % (Szwillus et al., 2019)
+
+topography = readTopo(topoFileName);
+mohoDepth = readMoho(mohoFileName);
+geoid = getGeoid();
+
+measuredThickness = mohoDepth * 1e3 - geoid + topography;
+
+bulkModulus = 128.8e9; % olivine, (Mao et al., 2015)
+shearModulus = 81.6e9; % olivine, (Mao er al., 2015)
+g0 = 9.80665;
+radius = 6371e3;
+mass = 3.986004418e14 / 6.6743e-11;
+referenceDepth = 24e3;
+
+% optimal values from SingleVariableOptimization
+baseMantleDensity = 3300;
+baseCrustDensity = 2780;
+baseElasticThickness = 17e3;
+
+% depthAlterations = -100:50:100;
+% sizeDepthAlterations = size(depthAlterations);
+densityAlterations = -150:5:-150;
+sizeDensityAlterations = size(densityAlterations);
+thicknessAlterations = -400:50:-400;
+sizeThicknessAlterations = size(thicknessAlterations);
+
+% depthIndexes = 1:sizeDepthAlterations(2);
+densityIndexes = 1:sizeDensityAlterations(2);
+thicknessIndexes = 1:sizeThicknessAlterations(2);
+
+minError = inf;
+minThickness = 0;
+
+% minReferenceDepth = 0;
+minMantleDensity = 0;
+minCrustDensity = 0;
+minElasticThickness = 0;
+
+% for depthIndex = depthIndexes
+%     referenceDepth = baseReferenceDepth + depthAlterations(depthIndex);
+for mantleIndex = densityIndexes
+    mantleDensity = baseMantleDensity + ...
+        densityAlterations(mantleIndex);
+    for crustIndex = densityIndexes
+        crustDensity = baseCrustDensity + ...
+            densityAlterations(crustIndex);
+        for thicknessIndex = thicknessIndexes
+            elasticThickness = baseElasticThickness + ...
+                thicknessAlterations(thicknessIndex);
+
+            crustGravity = calculateLowerCrustGravity(radius, ...
+                referenceDepth, crustDensity, mass);
+            airyCrust = airyEqualPressures(topography, crustDensity,...
+                mantleDensity, referenceDepth, g0, crustGravity);
+            flexureCrust = flexureThinShell(airyCrust, bulkModulus,...
+                shearModulus, elasticThickness, mantleDensity, ...
+                crustDensity, g0, radius);
+
+            error = rmse(flexureCrust, measuredThickness, "all");
+
+            if error < minError
+                minError = error;
+                minThickness = min(flexureCrust, [], "all");
+
+                % minReferenceDepth = referenceDepth;
+                minMantleDensity = mantleDensity;
+                minCrustDensity = crustDensity;
+                minElasticThickness = elasticThickness;
+            end
+        end
+    end
+end
+% end
+
+disp("Minimum error: " +  string(minError / 1e3) + " [km]")
+disp("Minimum crust thickness @ minimum error: " + string(minThickness / 1e3) + " [km]")
+% disp("Reference depth @ minimum error: " + string(minReferenceDepth / 1e3) + " [km]")
+disp("Mantle density @ minimum error: " + string(minMantleDensity) + " [kg/m3]")
+disp("Crust density @ minimum error: " + string(minCrustDensity) + " [kg/m3]")
+disp("Elastic thickness @ minimum error: " + string(minElasticThickness / 1e3) + " [km]")
+
+%% Found results:
+% Reference Depth = 24 km
+% Mantle Density = 3150 kg/m3
+% Crust Density = 2620 kg/m3
+% Elastic Thickness = 16.6 km
+% Minimum Error = 6.1337 km
---
+++ GSH-main/GSH-main/Thesis/Benchmark/SingleVariableOptimization.m
@@ -0,0 +1,247 @@
+clear;
+close all;
+clc;
+clearvars;
+
+addpath('..\..\Tools');
+addpath('..\SharedTools');
+
+topoFileName = "..\..\..\..\Data\Earth2014.BED2014.5min.geod.bin";  % (Hirt and Rexer, 2015)
+mohoFileName = "..\..\..\..\Data\MohoDepthSzwillusEtAl2019";  % (Szwillus et al., 2019)
+
+topography = readTopo(topoFileName);
+mohoDepth = readMoho(mohoFileName);
+geoid = getGeoid();
+
+measuredThickness = mohoDepth * 1e3 - geoid + topography;
+
+baseBulkModulus = 128.8e9; % olivine, (Mao et al., 2015)
+baseShearModulus = 81.6e9; % olivine, (Mao er al., 2015)
+baseElasticThickness = 34e3; % PPI slides
+
+baseMantleDensity = 3320; % olivine
+baseCrustDensity = 2760;
+g0 = 9.80665;
+radius = 6371e3;
+mass = 3.986004418e14 / 6.6743e-11;
+baseReferenceDepth = mean(measuredThickness, "all");
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% Reference Depth %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+depthAlterations = -2e3:100:2e3;
+sizeDepthAlterations = size(depthAlterations);
+errors = zeros(1,sizeDepthAlterations(2));
+minimumThicknesses = zeros(1,sizeDepthAlterations(2));
+indexes = 1:sizeDepthAlterations(2);
+
+for index = indexes
+    referenceDepth = baseReferenceDepth + depthAlterations(index);
+    crustGravity = calculateLowerCrustGravity(radius, ...
+        referenceDepth, baseCrustDensity, mass);
+    airyCrust = airyEqualPressures(topography, baseCrustDensity, ...
+        baseMantleDensity, referenceDepth, g0, crustGravity);
+    flexureCrust = flexureThinShell(airyCrust, baseBulkModulus,...
+        baseShearModulus, baseElasticThickness, baseMantleDensity, baseCrustDensity, ...
+        g0, radius);
+    errors(index) = rmse(flexureCrust, measuredThickness, "all");
+    minimumThicknesses(index) = min(flexureCrust, [], "all");
+end
+
+minError = min(errors, [], "all");
+minReferenceDepth = (baseReferenceDepth + depthAlterations(errors == minError)) / 1e3;
+disp("Minimum error: " +  string(minError / 1e3) + " [km]")
+disp("Reference Depth @ minimum error: " + string(minReferenceDepth) + " [km]")
+
+figure(1)
+yyaxis left
+plot((baseReferenceDepth + depthAlterations) / 1e3, errors / 1e3)
+xlabel("Reference Depth [km]")
+ylabel("RMSE [km]")
+yyaxis right
+plot((baseReferenceDepth + depthAlterations) / 1e3, minimumThicknesses / 1e3)
+ylabel("Minimum Crustal Thickness [km]")
+savefig("Images/SingleVariableOptimization/ReferenceDepthOptimization")
+saveas(gcf, "Images/SingleVariableOptimization/PNG/ReferenceDepthOptimization.png")
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Densities %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+densityAlterations = -200:20:200;
+sizeDensityAlterations = size(densityAlterations);
+
+%% mantle density
+errors = zeros(1,sizeDensityAlterations(2));
+minimumThicknesses = zeros(1,sizeDensityAlterations(2));
+indexes = 1:sizeDensityAlterations(2);
+
+for index = indexes
+    mantleDensity = baseMantleDensity + densityAlterations(index);
+    crustGravity = calculateLowerCrustGravity(radius, ...
+        baseReferenceDepth, baseCrustDensity, mass);
+    airyCrust = airyEqualPressures(topography, baseCrustDensity, ...
+        mantleDensity, baseReferenceDepth, g0, crustGravity);
+    flexureCrust = flexureThinShell(airyCrust, baseBulkModulus,...
+        baseShearModulus, baseElasticThickness, mantleDensity, baseCrustDensity, ...
+        g0, radius);
+    errors(index) = rmse(flexureCrust, measuredThickness, "all");
+    minimumThicknesses(index) = min(flexureCrust, [], "all");
+end
+
+minError = min(errors, [], "all");
+minMantleDensity = (baseMantleDensity + densityAlterations(errors == minError));
+disp("Minimum error: " +  string(minError / 1e3) + " [km]")
+disp("Mantle Density @ minimum error: " + string(minMantleDensity) + " [kg/m3]")
+
+figure(2)
+yyaxis left
+plot((baseMantleDensity + densityAlterations), errors / 1e3)
+xlabel("Mantle Density [kg/m^{3}]")
+ylabel("RMSE [km]")
+yyaxis right
+plot((baseMantleDensity + densityAlterations), minimumThicknesses / 1e3)
+ylabel("Minimum Crustal Thickness [km]")
+savefig("Images/SingleVariableOptimization/MantleDensityOptimization")
+saveas(gcf, "Images/SingleVariableOptimization/PNG/MantleDensityOptimization.png")
+
+%% crust density
+errors = zeros(1,sizeDensityAlterations(2));
+minimumThicknesses = zeros(1,sizeDensityAlterations(2));
+indexes = 1:sizeDensityAlterations(2);
+
+for index = indexes
+    crustDensity = baseCrustDensity + densityAlterations(index);
+    crustGravity = calculateLowerCrustGravity(radius, ...
+        baseReferenceDepth, crustDensity, mass);
+    airyCrust = airyEqualPressures(topography, crustDensity, ...
+        baseMantleDensity, baseReferenceDepth, g0, crustGravity);
+    flexureCrust = flexureThinShell(airyCrust, baseBulkModulus,...
+        baseShearModulus, baseElasticThickness, baseMantleDensity, crustDensity, ...
+        g0, radius);
+    errors(index) = rmse(flexureCrust, measuredThickness, "all");
+    minimumThicknesses(index) = min(flexureCrust, [], "all");
+end
+
+minError = min(errors, [], "all");
+minCrustDensity = (baseCrustDensity + densityAlterations(errors == minError));
+disp("Minimum error: " +  string(minError / 1e3) + " [km]")
+disp("Crust Density @ minimum error: " + string(minCrustDensity) + " [kg/m3]")
+
+figure(3)
+yyaxis left
+plot((baseCrustDensity + densityAlterations), errors / 1e3)
+xlabel("Crust Density [kg/m^{3}]")
+ylabel("RMSE [km]")
+yyaxis right
+plot((baseCrustDensity + densityAlterations), minimumThicknesses / 1e3)
+ylabel("Minimum Crustal Thickness [km]")
+savefig("Images/SingleVariableOptimization/CrustDensityOptimization")
+saveas(gcf, "Images/SingleVariableOptimization/PNG/CrustDensityOptimization.png")
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% Elastic Thickness %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+thicknessAlterations = -24e3:1e3:6e3;
+sizeThicknessAlterations = size(thicknessAlterations);
+errors = zeros(1,sizeThicknessAlterations(2));
+minimumThicknesses = zeros(1,sizeThicknessAlterations(2));
+indexes = 1:sizeThicknessAlterations(2);
+
+for index = indexes
+    elasticThickness = baseElasticThickness + thicknessAlterations(index);
+    crustGravity = calculateLowerCrustGravity(radius, ...
+        baseReferenceDepth, baseCrustDensity, mass);
+    airyCrust = airyEqualPressures(topography, baseCrustDensity, ...
+        baseMantleDensity, baseReferenceDepth, g0, crustGravity);
+    flexureCrust = flexureThinShell(airyCrust, baseBulkModulus,...
+        baseShearModulus, elasticThickness, baseMantleDensity, baseCrustDensity, ...
+        g0, radius);
+    errors(index) = rmse(flexureCrust, measuredThickness, "all");
+    minimumThicknesses(index) = min(flexureCrust, [], "all");
+end
+
+minError = min(errors, [], "all");
+minElasticThickness = (baseElasticThickness + thicknessAlterations(errors == minError)) / 1e3;
+disp("Minimum error: " +  string(minError / 1e3) + " [km]")
+disp("Elastic Thickness @ minimum error: " + string(minElasticThickness) + " [km]")
+
+figure(4)
+yyaxis left
+plot((baseElasticThickness + thicknessAlterations) / 1e3, errors / 1e3)
+xlabel("Elastic Thickness [km]")
+ylabel("RMSE [km]")
+yyaxis right
+plot((baseElasticThickness + thicknessAlterations) / 1e3, minimumThicknesses / 1e3)
+ylabel("Minimum Crustal Thickness [km]")
+savefig("Images/SingleVariableOptimization/ElasticThicknessOptimization")
+saveas(gcf, "Images/SingleVariableOptimization/PNG/ElasticThicknessOptimization.png")
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Moduli %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+modulusAlterations = -50e9:50e8:50e9;
+sizeModulusAlterations = size(modulusAlterations);
+
+%% bulk modulus
+errors = zeros(1,sizeModulusAlterations(2));
+minimumThicknesses = zeros(1,sizeModulusAlterations(2));
+indexes = 1:sizeModulusAlterations(2);
+
+for index = indexes
+    bulkModulus = baseBulkModulus + modulusAlterations(index);
+    crustGravity = calculateLowerCrustGravity(radius, ...
+        baseReferenceDepth, baseCrustDensity, mass);
+    airyCrust = airyEqualPressures(topography, baseCrustDensity, ...
+        baseMantleDensity, baseReferenceDepth, g0, crustGravity);
+    flexureCrust = flexureThinShell(airyCrust, bulkModulus,...
+        baseShearModulus, baseElasticThickness, baseMantleDensity, baseCrustDensity, ...
+        g0, radius);
+    errors(index) = rmse(flexureCrust, measuredThickness, "all");
+    minimumThicknesses(index) = min(flexureCrust, [], "all");
+end
+
+minError = min(errors, [], "all");
+minBulkModulus = (baseBulkModulus + modulusAlterations(errors == minError)) / 1e9;
+disp("Minimum error: " +  string(minError / 1e3) + " [km]")
+disp("Bulk Modulus @ minimum error: " + string(minBulkModulus) + " [GPa]")
+
+figure(5)
+yyaxis left
+plot((baseBulkModulus + modulusAlterations) / 1e9, errors / 1e3)
+xlabel("Bulk Modulus [GPa]")
+ylabel("RMSE [km]")
+yyaxis right
+plot((baseBulkModulus + modulusAlterations) / 1e9, minimumThicknesses / 1e3)
+ylabel("Minimum Crustal Thickness [km]")
+savefig("Images/SingleVariableOptimization/BulkModulusOptimization")
+saveas(gcf, "Images/SingleVariableOptimization/PNG/BulkModulusOptimization.png")
+
+%% crust density
+errors = zeros(1,sizeModulusAlterations(2));
+minimumThicknesses = zeros(1,sizeModulusAlterations(2));
+indexes = 1:sizeModulusAlterations(2);
+
+for index = indexes
+    shearModulus = baseShearModulus + modulusAlterations(index);
+    crustGravity = calculateLowerCrustGravity(radius, ...
+        baseReferenceDepth, baseCrustDensity, mass);
+    airyCrust = airyEqualPressures(topography, baseCrustDensity, ...
+        baseMantleDensity, baseReferenceDepth, g0, crustGravity);
+    flexureCrust = flexureThinShell(airyCrust, baseBulkModulus,...
+        shearModulus, baseElasticThickness, baseMantleDensity, baseCrustDensity, ...
+        g0, radius);
+    errors(index) = rmse(flexureCrust, measuredThickness, "all");
+    minimumThicknesses(index) = min(flexureCrust, [], "all");
+end
+
+minError = min(errors, [], "all");
+minShearModulus = (baseShearModulus + modulusAlterations(errors == minError)) / 1e9;
+disp("Minimum error: " +  string(minError / 1e3) + " [km]")
+disp("Shear Modulus @ minimum error: " + string(minShearModulus) + " [GPa]")
+
+figure(6)
+yyaxis left
+plot((baseShearModulus + modulusAlterations) / 1e9, errors / 1e3)
+xlabel("Shear Modulus [GPa]")
+ylabel("RMSE [km]")
+yyaxis right
+plot((baseShearModulus + modulusAlterations) / 1e9, minimumThicknesses / 1e3)
+ylabel("Minimum Crustal Thickness [km]")
+savefig("Images/SingleVariableOptimization/ShearModulusOptimization")
+saveas(gcf, "Images/SingleVariableOptimization/PNG/ShearModulusOptimization.png")
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
