9aa3ca7e6f3f0149f2dfb4b9a230e80848f21d3d
jjlems <j.j.lems@student.tudelft.nl>
2024-08-17 15:58:33 +0200

    Finetuned visualization

--- ThermalProfile/ThermalProfiles.py
+++ ThermalProfile/ThermalProfiles.py
@@ -3,8 +3,10 @@ sys.path.append("../Differentiation")
 from Tools import *
 from Differentiation.Tools import *
 
-def thermalProfile(rlist):
-    c1crust, c2crust, c2mantle = continuityThermalProfileCoefficients()
+def thermalProfile(rlist, mantleHeatGeneration):
+    crustHeatGeneration = crustRockFraction * mantleHeatGeneration
+
+    c1crust, c2crust, c2mantle = continuityThermalProfileCoefficients(mantleHeatGeneration, crustHeatGeneration)
 
     crustList = rlist[rlist >= boundaryRadius]
     mantleList = rlist[rlist < boundaryRadius]
@@ -20,44 +22,45 @@ def thermalProfile(rlist):
 if __name__ == "__main__":
     dr = 5e3
     rlist = np.arange(0, 471e3, dr)
-    temperatures = thermalProfile(rlist)
-    print(temperatures)
+    temperatures1 = thermalProfile(rlist, 0.8e-14)
+    temperatures2 = thermalProfile(rlist, 1.05e-14)
+    temperatures3 = thermalProfile(rlist, 1.3e-14)
 
     densityProfile = np.array([mantleDensity] * len(rlist[rlist < boundaryRadius]) +
                               [crustDensity] * len(rlist[rlist >= boundaryRadius]))
 
     pressures = profileCalculator(rlist, densityProfile, dr)[2]
 
-    phaseDiagramPressures = np.array([1, 2, 5, 10, 15] + list(np.arange(20, 110, 10)) + list(np.arange(120, 220, 20)))
+    phaseDiagramPressures = np.array([1, 2, 5, 10, 15] + list(np.arange(20, 110, 10)) + list(np.arange(120, 180, 20)))
     phaseDiagramTemperatures = np.array([-0.064, -0.14, -0.37, -0.75, -1.14, -1.54, -2.36, -3.21, -4.09, -5, -5.94,
-                                         -6.91, -7.91, -8.94, -11.09, -13.35, -15.73, -18.22, -20.83]) + 273.15
-    phaseDiagramTemperatures -= 1.86 * 2 * 360 / 58.443 / 917
+                                         -6.91, -7.91, -8.94, -11.09, -13.35, -15.73]) + 273.15
+                                        # Engineering Toolbox
+    phaseDiagramTemperatures1 = (phaseDiagramTemperatures - freezingDepressionConstant * NaClSolutionParticles * 180e3
+                                 / NaClMolarMass / iceDensity)
+    phaseDiagramTemperatures2 = (phaseDiagramTemperatures - freezingDepressionConstant * NaClSolutionParticles * 360e3
+                                 / NaClMolarMass / iceDensity)
 
-    matplotlib.rcParams.update({'font.size': 18})
-    plt.figure(figsize=(6, 6))
-    plt.xlabel("Temperature [K]")
-    plt.ylabel("Radius [km]")
-    plt.plot(temperatures, rlist / 1e3)
-    plt.tight_layout()
-    plt.savefig(r"Images/TemperatureProfile.pdf")
-    plt.savefig(r"Images/TemperatureProfile.png")
-    plt.show()
+    matplotlib.rcParams.update({'font.size': 16})
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+    fig.supxlabel("Temperature [K]")
+    axs[0].set_ylabel("Radius [km]")
+    axs[0].plot(temperatures1, rlist / 1e3)
+    axs[0].plot(temperatures2, rlist / 1e3)
+    axs[0].plot(temperatures3, rlist / 1e3)
 
-    matplotlib.rcParams.update({'font.size': 18})
-    plt.figure(figsize=(6, 6))
-    plt.xlabel("Pressure [MPa]")
-    plt.ylabel("Radius [km]")
-    plt.plot(pressures / 1e6, rlist / 1e3)
-    plt.tight_layout()
-    plt.show()
+    axs[1].set_ylabel("Pressure [MPa]")
+    axs[1].plot(temperatures1, pressures / 1e6, label = r"H=0.8$\cdot 10^{-14}$")
+    axs[1].plot(temperatures2, pressures / 1e6, label = r"H=1.05$\cdot 10^{-14}$")
+    axs[1].plot(temperatures3, pressures / 1e6, label = r"H=1.3$\cdot 10^{-14}$")
+    axs[1].plot(phaseDiagramTemperatures, phaseDiagramPressures, label = "0 g/L NaCl")
+    axs[1].plot(phaseDiagramTemperatures1, phaseDiagramPressures, label = "180 g/L NaCl")
+    axs[1].plot(phaseDiagramTemperatures2, phaseDiagramPressures, label = "360 g/L NaCl")
+    axs[1].axhline(22.5, color = "grey", linestyle = '--')
+    plt.gca().invert_yaxis()
 
-    matplotlib.rcParams.update({'font.size': 18})
-    plt.figure(figsize=(6, 6))
-    plt.xlabel("Temperature [K]")
-    plt.ylabel("Pressure [MPa]")
-    plt.plot(temperatures, pressures / 1e6)
-    plt.plot(phaseDiagramTemperatures, phaseDiagramPressures)
+    handles, labels = plt.gca().get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper right')
     plt.tight_layout()
-    plt.savefig(r"Images/PTdiagram.pdf")
-    plt.savefig(r"Images/PTdiagram.png")
+    plt.savefig(r"Images/Temperature.pdf")
+    plt.savefig(r"Images/Temperature.png")
     plt.show()
--- ThermalProfile/Tools.py
+++ ThermalProfile/Tools.py
@@ -10,52 +10,45 @@ import scipy.optimize
 # constants
 R = 470e3  # [m], park 2016
 boundaryRadius = R - 70e3
-emissivity = 0.91
+albedo = 0.09 # (Li et al., 2006)
+emissivity = 1 - albedo
 StefanBoltzmannConstant = 5.670374419e-8
-surfaceTemperature = (1.37e3 / 2.77 ** 2 * (1 - 0.09) / (4 * emissivity * StefanBoltzmannConstant)) ** (1/4)
+solarConstant = 1.37e3 # (Imke de Pater & Jack J. Lissauer, Fundamental Planetary Science)
+semiMajorAxis = 2.77 # AU (Imke de Pater & Jack J. Lissauer, Fundamental Planetary Science)
+surfaceTemperature = ((solarConstant / semiMajorAxis ** 2 * (1 - albedo) / (4 * emissivity * StefanBoltzmannConstant))
+                      ** (1/4))
 
 serpentiniteConductivity = 2.7  # (Osako et al., 2010)
 serpentiniteHeatCapacity = 1e3  # (Osako et al., 2010)
+serpentiniteDensity = 2600
 
 iceConductivity = 3.48  # Engineering Toolbox
 iceHeatCapacity = 1389  # Engineering Toolbox
+iceDensity = 917
 
-crustDensity = 1570
-crustRockFraction = (crustDensity - 917) / (2600 - 917)
+crustDensity = 1570 # From crust modelling
+crustRockFraction = (crustDensity - iceDensity) / (serpentiniteDensity - iceDensity)
 
 crustConductivity = crustRockFraction * serpentiniteConductivity + (1 - crustRockFraction) * iceConductivity
 crustHeatCapacity = crustRockFraction * serpentiniteHeatCapacity + (1 - crustRockFraction) * iceHeatCapacity
 k_crust = crustConductivity / crustDensity / crustHeatCapacity
 
 mantleConductivity = serpentiniteConductivity
-mantleDensity = 2520
+mantleDensity = 2520 # From crust modelling
 mantleHeatCapacity = serpentiniteHeatCapacity
 k_mantle = mantleConductivity / mantleDensity / mantleHeatCapacity
 
-# heatGeneration = (26.4e-6 * 2.96e-8 * 0.5 ** (4.5e9/14e9)
-#                   + 94.6e-6 * 8.08e-9 * 0.5 ** (4.5e9/4.47e9)
-#                   + 29.2e-6 * 5.53e-4 * 0.5 ** (4.5e9/1.25e9)
-#                   + 569e-6 * 8.08e-9 * 0.5 ** (4.5e9/704e6))
+freezingDepressionConstant = 1.86 # Aylward, Gordon; Findlay, Tristan (2002), SI Chemical Data 5th ed.
+NaClSolutionParticles = 2
+NaClMolarMass = 58.443
+
+# heatGeneration = serpentiniteDensity * (64e-9 * 1.69e-12 * 0.5 ** (4.5e9/14e9)
+#                   + 16e-9 * 1.5e-12 * 0.5 ** (4.5e9/4.47e9)
+#                   + 160e-6 * 5.56e-12 * 0.5 ** (4.5e9/1.25e9)
+#                   + 16e-9 * 6.46e-14 * 0.5 ** (4.5e9/704e6)) # Treimann, 1986; Grott Breuer, 2008
 # print(heatGeneration)
 
-mantleHeatGeneration = 1.2e-14
-crustHeatGeneration = crustRockFraction * mantleHeatGeneration
-
-# def radiationThermalProfileCoefficients():
-#     # calculates coefficients for the thermal profile given the constraint of outgoing thermal radiation
-#     # T(r) = - (Hr^2 / 6k) - (c1 / kr) + c2
-#     c1crust = k_crust * R ** 2 * (heatGeneration * R / (3 * k_crust) + emissivity * StefanBoltzmannConstant
-#                               * surfaceTemperature ** 4)
-#     c2crust = surfaceTemperature + heatGeneration * R ** 2 / (6 * k_crust) + c1crust / (k_crust * R)
-#
-#     boundaryTemperature = (- heatGeneration * boundaryRadius ** 2 / (6 * k_crust) - c1crust / (k_crust * boundaryRadius)
-#                            + c2crust)
-#
-#     c2mantle = boundaryTemperature + heatGeneration * boundaryRadius ** 2 / (6 * k_mantle)
-#
-#     return c1crust, c2crust, c2mantle
-
-def continuityThermalProfileCoefficients():
+def continuityThermalProfileCoefficients(mantleHeatGeneration, crustHeatGeneration):
     # calculates coefficients for the thermal profile give the constraint of continuity of the gradients between layers
     # T(r) = - (Hr^2 / 6k) - (c1 / kr) + c2
     c1crust = boundaryRadius ** 3 / 3 * (crustHeatGeneration - k_crust / k_mantle * mantleHeatGeneration)
